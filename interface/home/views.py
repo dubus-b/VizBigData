b9f2e0f39a820fdae0563e9f4cf72c57912c3a07
Benjamin DUBUS <benjamin.dubus@epitech.eu>
2020-02-27 17:52:11 +0100

    render on index div

--- interface/home/views.py
+++ interface/home/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 import plotly.graph_objects as go
 import numpy as np
+import plotly.offline as opy
+import plotly.express as px
 
 
 import pandas as pd
 
+#fig = go.Figure(data=go.Scatter(x=data['math score'], y=data['reading score'], mode='markers', marker_color=data['gender']))
 
 # Create your views here.
 
 def index(request):
     # Nous utilisons pandas pour faciliter la lecture des données du csv
     data = pd.read_csv('data.csv')
-    print(data['math score'])
-    fig = go.Figure(data=go.Scatter(x=data['math score'], y=data['reading score'], mode='markers'), color=data['gender'])
-    fig.show()    
-    return TemplateResponse(request, 'index.html', {'data' : data})
+    figo = px.scatter(data, x="math score", y="reading score", color="lunch",
+                 hover_data=['lunch'])
+    div = opy.plot(figo, output_type='div')
+    return TemplateResponse(request, 'index.html', {'data' : div})
