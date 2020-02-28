d38593a2d8a561509ef53e579ff96865b56254de
PaulF <paul.faucheur@epitech.eu>
2020-02-28 17:39:38 +0100

    real values in parallel coordinates

--- interface/home/views.py
+++ interface/home/views.py
@@ -6,6 +6,7 @@ import plotly.offline as opy
 import plotly.express as px
 import pandas as pd
 import plotly.express as px
+import plotly as py
 
 data = pd.read_csv('data.csv')
 
@@ -17,7 +18,7 @@ def get_pie_chart():
     #fig = go.Figure(data=go.Scatter(x=data['math score'], y=data['reading score'], mode='markers', marker_color=data['gender']))
 
     # Create your views here.
-
+#labels={'sexe':data["gender"], 'Writing': "writing score", 'Reading': "reading score", 'Math': "math score",}
 
 def index(request):
     # Nous utilisons pandas pour faciliter la lecture des données du csv
@@ -27,7 +28,8 @@ def index(request):
                        yaxis_zeroline=False, xaxis_zeroline=False)
     div = opy.plot(figo, output_type='div')
 
-    figu = px.parallel_coordinates(data, labels={'':''}, color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
+    figu = px.parallel_coordinates(data, dimensions=["gender", "writing score", "reading score", "math score"],
+                        color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
     div2 = opy.plot(figu, output_type='div')
 
     return TemplateResponse(request, 'index.html', {'data' : div, 'data2': div2, 'pie' : get_pie_chart()})
